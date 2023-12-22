77fc8dbbb96af26dd5ffe20241043c959f6ee9cd
kelegele <felicis1024@gmail.com>
2023-12-22 20:22:10 +0000

    Deploying to gh-pages from @ kelegele/blog.kelegele.com@e08a59f2a59d18cbd4efc622bfd7ec2802a2894c 🚀

--- assets/pageview-4d9Le-5s.js
+++
@@ -1,7 +0,0 @@
-import{a4 as t}from"./app-D47_Z1PS.js";var a={provider:"Waline",dark:"auto",serverURL:"https://blog-comment-kelegele.vercel.app/",requiredMeta:["nick"],login:"force",wordLimit:[5,500]};const i=async()=>{try{const{pageviewCount:e}=await t(()=>import("./app-D47_Z1PS.js").then(r=>r.a5),__vite__mapDeps([]));return e({serverURL:a.serverURL})}catch{console.error("@waline/client is not installed!");return}};export{i as updatePageview};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
